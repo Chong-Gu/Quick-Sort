4b68d648eb9b8242ba49c5f0039b9e88d087e367
Chong-Gu <chonggu24@gmail.com>
2020-07-13 17:10:48 -0700

    first

---
+++ src/quick/quick_sort.java
@@ -0,0 +1,15 @@
+package quick;
+
+public class quick_sort {
+	public static void main (String[] arg) {
+		int[] arr = {5, 1, 6, 2, 3, 4};
+		printArray(arr);
+	}
+	
+	private static void printArray(int[] arr) {
+		for (int i: arr) 
+			System.out.print(i + " ");
+		
+		System.out.print("\n");
+	}
+}
